1727c841fb47a16705157876e127c834e4180eb4
Anderson Afonso <aafonso56@gmail.com>
2022-01-11 18:38:27 -0300

    Alterações no inicio da aplicação

--- src/components/Body.js
+++ src/components/Body.js
@@ -9,7 +9,10 @@ import { useState, useEffect } from 'react';
 const Body = ()=>{
     const [cityField, setCityField] = useState('');
     const [cityData, setCityData] = useState([]);
-    const [showTitle, setShowTitle] = useState(true)
+    const [showTitle, setShowTitle] = useState(true);
+    const [brasiliaCity, setBrasiliaCity] = useState([])
+    const [LondonCity, setLondonCity] = useState([])
+    const [newYorkCity, setNewYorkCity] = useState([])
 
     const getCity = async(e)=>{
         e.preventDefault()
@@ -19,15 +22,52 @@ const Body = ()=>{
         setCityField('')
         setShowTitle(false)
     }
+    const getBrasilia = async()=>{
+        let res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=brasilia&&units=metric&lang=pt&appid=e6589e7279a8a0ec755503aff3d4f22d`)
+        let json = res.data;
+        setBrasiliaCity([json])
+    }
+    const getLondon = async()=>{
+        let res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=london&&units=metric&lang=pt&appid=e6589e7279a8a0ec755503aff3d4f22d`)
+        let json = res.data;
+        setLondonCity([json])
+    }
+    const getNewYork = async()=>{
+        let res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=nova%20iorque&&units=metric&lang=pt&appid=e6589e7279a8a0ec755503aff3d4f22d`)
+        let json = res.data;
+        setNewYorkCity([json])
+    }
     useEffect(() => {
-        getCity()
+        getCity();
+        getBrasilia();
+        getLondon();
+        getNewYork();
     }, [])
 
     return(
         <div className="body">
             <div className='box'>
                 {showTitle && 
-                    <h3>Insira abaixo sua cidade!</h3>
+                    <div>   
+                        <div className='citiesTemp'>
+                            {brasiliaCity.map((item)=>
+                                 <h4>{item.name} - {item.main.temp.toFixed(1)}°</h4>
+                            )}
+                        
+                        
+                            {LondonCity.map((item)=>
+                                 <h4>{item.name} - {item.main.temp.toFixed(1)}°</h4>
+                            )}
+                        
+                        
+                            {newYorkCity.map((item)=>
+                                 <h4>{item.name} - {item.main.temp.toFixed(1)}°</h4>
+                            )}
+                        </div>
+                        
+                        <h3>Insira abaixo sua cidade!</h3>
+                    </div>    
+                    
                 }
                 
                 {cityData.map((item, index)=>(
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import './header.css'
-
+import Cloud from '../images/cloud.png'
 const Header = ()=>{
     return(
         <div className="header">
-            <h1>Seu Clima</h1>
+            <h1>SeuClima.com</h1>
+            <img className='cloud' src={Cloud}/>
         </div>
     )
 };
